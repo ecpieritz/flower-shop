541cfa203bcfa2d7bb7ab730423c5257d1b7bcdc
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-06-15 07:28:25 -0300

    Added script base

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+!(function($) {
+  "use strict";
+
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  
+
+  // Activate smooth scroll on page load with hash links in the url
+  
+
+  // Mobile Navigation
+  
+
+  // Navigation active state on scroll
+  
+
+  // Toggle .header-scrolled class to #header when page is scrolled
+  
+
+  // Back to top button
+  
+
+  // jQuery counterUp
+  
+
+  // Porfolio isotope and filter
+  
+
+  // Initiate venobox (lightbox feature used in portofilo)
+  
+
+  // Testimonials carousel (uses the Owl Carousel library)
+  
+
+  // Portfolio details carousel
+  
+
+  // Init AOS
+  
+
+})(jQuery);
